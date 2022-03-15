1c1d5cb7ea86077158f9ce237221097edf3f1b7d
Laras <96417338+Larasatilaras@users.noreply.github.com>
2022-03-15 18:22:48 +0700

    Update banuser.js

--- plugins/banuser.js
+++ plugins/banuser.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, text }) => {
-    if (!text) throw '_*Siape Yang Mo Koko Pangeran Entod❓*_'
+    if (!text) throw '_*Siape Yang Mo Araa Entod❓*_'
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
     if (!who) throw 'Tag salah satu lah'
     let users = global.db.data.users
     users[who].banned = true
-    conn.reply(m.chat, `_*Asik Berhasil Koko Pangeran Entod😗*_`, m)
+    conn.reply(m.chat, `_*Asik Berhasil Araa Entod😗*_`, m)
 }
 handler.help = ['ban']
 handler.tags = ['owner']
