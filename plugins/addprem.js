b29b2367add713b40ec0cc353a978094fbcbebe7
Laras <96417338+Larasatilaras@users.noreply.github.com>
2022-03-15 18:17:33 +0700

    Update addprem.js

--- plugins/addprem.js
+++ plugins/addprem.js
@@ -2,10 +2,10 @@ let handler = async (m, { conn, text }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
     else who = m.chat
-    if (!who) throw `_*Pilih Wanita Nya!*_`
+    if (!who) throw `_*Pilih Pria Nya!*_`
     if (global.prems.includes(who.split`@`[0])) throw 'dia udah premium!'
     global.prems.push(`${who.split`@`[0]}`)
-    conn.reply(m.chat, `@${who.split`@`[0]} Sekarang Kamu Jadi Istri Pangeran!`, m, {
+    conn.reply(m.chat, `@${who.split`@`[0]} Sekarang Kamu Jadi Suami Araa!`, m, {
         contextInfo: {
             mentionedJid: [who]
         }
