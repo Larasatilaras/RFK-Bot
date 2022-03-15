67ef3cc36ecb575f3bc4a9e3c6521b0c81214ab0
Laras <96417338+Larasatilaras@users.noreply.github.com>
2022-03-15 18:26:32 +0700

    Update delprem.js

--- plugins/delprem.js
+++ plugins/delprem.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, text }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
     else who = m.chat
-    if (!who) throw `_*Wanita Mana Yang Pangeran Ingin Ceraikan😌*_`
-    if (!global.prems.includes(who.split`@`[0])) throw '_*Wanita Itu Bukan Istri Pangeran!*_'
+    if (!who) throw `_*Pria Mana Yang Araa Ingin Ceraikan😌*_`
+    if (!global.prems.includes(who.split`@`[0])) throw '_*Pria Itu Bukan Suami Araa!*_'
     let index = global.prems.findIndex(v => (v.replace(/[^0-9]/g, '') + '@s.whatsapp.net') === (who.replace(/[^0-9]/g, '') + '@s.whatsapp.net'))
     global.prems.splice(index, 1)
-    conn.reply(m.chat, `@${who.split('@')[0]} Sekarang Kamu Bukan Istri Pangeran!`, m, {
+    conn.reply(m.chat, `@${who.split('@')[0]} Sekarang Kamu Bukan Suami Araa!`, m, {
         contextInfo: {
             mentionedJid: [who]
         }
